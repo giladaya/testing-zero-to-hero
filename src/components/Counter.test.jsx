3160541d89233984b7f5c7c9e3676f7ee1d5a9ee
giladd <giladd@trainologic.com>
2017-07-12 20:32:47 +0300

    add DOM tests

--- src/components/Counter.test.jsx
+++ src/components/Counter.test.jsx
@@ -2,10 +2,32 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Counter from './Counter.jsx';
 
-it('renders without crashing', () => {
+describe('Smoke tests', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+      <Counter/>,
+      div
+    );
+  });
+})
+
+describe('DOM tests', () => {
   const div = document.createElement('div');
   ReactDOM.render(
-    <Counter/>,
+    <Counter min={2} max={11}/>,
     div
   );
-});
+
+  it('renders one span', () => {
+    expect(div.querySelectorAll('span').length).toEqual(1);
+  });
+
+  it('renders min value', () => {
+    expect(div.querySelectorAll('span')[0].innerHTML).toEqual('2');
+  });
+
+  it('renders button', () => {
+    expect(div.querySelectorAll('button').length).toEqual(2);
+  });
+})
