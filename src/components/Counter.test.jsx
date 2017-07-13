8080416fb011e8a69e980c746d9d6ff858746757
giladd <giladd@trainologic.com>
2017-07-13 08:49:51 +0300

    add logic tests

--- src/components/Counter.test.jsx
+++ src/components/Counter.test.jsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Counter from './Counter.jsx';
+import { mount } from 'enzyme';
 
 describe('Smoke tests', () => {
   it('renders without crashing', () => {
@@ -29,5 +30,42 @@ describe('DOM tests', () => {
 
   it('renders button', () => {
     expect(div.querySelectorAll('button').length).toEqual(2);
+  });
+})
+
+describe("Behavior tests", () => {
+  let fixture;
+
+  beforeEach(() => {
+    fixture = mount(
+      <Counter min={2} max={11}/>
+    );
+  });
+
+  it('dec by 1 on minus click', () => {
+    fixture.setState({count: 5});
+    const minusBtn = fixture.find('button').at(0);
+    minusBtn.simulate('click');
+    expect(fixture.state().count).toBe(4);
+  });
+
+  it('inc by 1 on plus click', () => {
+    const plusBtn = fixture.find('button').at(1);
+    plusBtn.simulate('click');
+    expect(fixture.state().count).toBe(3);
+  });
+
+  it('does not go below min', () => {
+    fixture.setState({count: 2});
+    const minusBtn = fixture.find('button').at(0);
+    minusBtn.simulate('click');
+    expect(fixture.state().count).toBe(2);
+  });
+
+  it('does not go above max', () => {
+    fixture.setState({count: 11});
+    const plusBtn = fixture.find('button').at(1);
+    plusBtn.simulate('click');
+    expect(fixture.state().count).toBe(11);
   });
 })
